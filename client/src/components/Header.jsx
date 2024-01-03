27f10541e4c9aeac98bd733eaffaa1335c6fbbad
Abhishek <abhitiku2003@gmail.com>
2024-01-03 21:45:48 +0530

    home and  about

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,6 +22,8 @@ export default function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
             <Link to='/sign-in'>
             <li className=' text-slate-700 hover:underline'>Sign in</li></Link>
+            <Link to='/sign-up'>
+            <li className=' text-slate-700 hover:underline'>Sign up</li></Link>
         </ul>
 
         </div>
