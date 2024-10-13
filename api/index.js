b2b4cc9b257b92ed8660291ed1d123588e5a2f22
Ansh Rawat <ansh2k3@gmail.com>
2024-10-13 23:40:23 +0530

    update index

--- api/index.js
+++ api/index.js
@@ -5,39 +5,43 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-dotenv.config();
 import cors from 'cors';
 
-mongoose.connect(process.env.VITE_MONGO).then(()=>{
-  console.log('Connected to MongoDB!');
-    }).catch((err)=>{
-      console.log(err);
-    });
+dotenv.config();
 
+mongoose.connect(process.env.VITE_MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 app.use(cors());
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000!');
+// Health route
+app.head('/health', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
 
-  app.use("/api/user", userRouter);
-  app.use("/api/auth", authRouter);
-  app.use("/api/listing", listingRouter);
-
-  app.use((err, req ,res, next) =>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
-  });
-
-    
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
